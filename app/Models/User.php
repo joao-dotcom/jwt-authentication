b0203bf4fca74006acd9fa1fed78e03a549bab66
joao-dotcom <r6joao@gmail.com>
2026-03-15 16:54:03 -0300

    autenticação com jwt completa

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,23 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
     /**
      * The attributes that are mass assignable.
      *
